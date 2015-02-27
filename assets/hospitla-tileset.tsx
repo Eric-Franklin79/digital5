50427be804a6a7100eb12f62d65c40028da16a84
Eric-Franklin79 <pickle797900@verizon.net>
2015-02-27 18:16:19 -0500

    new

---
+++ assets/hospitla-tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="hospitla-tileset" tilewidth="16" tileheight="16">
+ <image source="hospitla-tileset.png" trans="000000" width="144" height="300"/>
+</tileset>
---
+++ js/main.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+    
+    "use strict";
+    
+    
+    var game = new Phaser.Game( 840, 640, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update} );
+    
+    function preload() {
+
+    }
+    
+    
+   function create(){
+   	  
+   }
+    
+    function update() {
+    	   
+    }
+    
+};
